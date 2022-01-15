a44a2773c8aa683e8f030c3687627f0f992b4af7
Peng Xiao <pengxiao@outlook.com>
2022-01-15 23:34:40 +0800

    feat: add trace opt

--- publish.mjs
+++ publish.mjs
@@ -5,72 +5,75 @@ import { hideBin } from "yargs/helpers";
 import path from "path";
 import fs from "fs";
 
-const argv = yargs(hideBin(process.argv))
-  .option("path", {
-    alias: "p",
-    type: "string",
-  })
-  .parse();
-
-const traceFile = "trace.zip";
-
-// @ts-ignore
-const graphPath = path.resolve(process.cwd(), argv.path);
-const graphDistPath = path.resolve(process.cwd(), graphPath + "-www");
-
-let graphFolderExists = false;
-
-try {
-  if (graphPath && fs.statSync(graphPath).isDirectory()) {
-    graphFolderExists = true;
-  }
-} catch (err) {
-  //
-}
-
-let distPathExists = false;
-
-function checkGraphDistPathExist() {
-  try {
-    if (
-      distPathExists ||
-      (graphDistPath &&
-        fs.statSync(path.join(graphDistPath, "static")).isDirectory())
-    ) {
-      if (!distPathExists) {
-        console.log("checkGraphDistPathExist: true");
-      }
-      distPathExists = true;
-      return true;
-    }
-  } catch (err) {
-    console.log("checkGraphDistPathExist: false");
-    return false;
-  }
-}
-
-function checkGraphPublishing() {
-  try {
-    if (
-      fs
-        .statSync(path.join(graphDistPath, "static", "js", "publishing"))
-        .isDirectory()
-    ) {
-      console.log("publishing ...");
-      return true;
-    }
-  } catch (err) {
-    console.log("publishing ... done");
-    return false;
-  }
-}
-
-if (!graphFolderExists) {
-  console.log(`Provided graph folder ${graphPath} doesn't exist!`);
-  process.exit(1);
-}
-
 async function main() {
+  const argv = await yargs(hideBin(process.argv))
+    .option("path", {
+      alias: "p",
+      type: "string",
+    })
+    .option("trace", {
+      alias: "t",
+      type: "boolean",
+    })
+    .parse();
+
+  const graphPath = path.resolve(process.cwd(), argv.path);
+  const graphDistPath = path.resolve(process.cwd(), graphPath + "-www");
+
+  const traceFile = path.join(graphDistPath, "trace.zip");
+
+  let graphFolderExists = false;
+
+  try {
+    if (graphPath && fs.statSync(graphPath).isDirectory()) {
+      graphFolderExists = true;
+    }
+  } catch (err) {
+    //
+  }
+
+  let distPathExists = false;
+
+  function checkGraphDistPathExist() {
+    try {
+      if (
+        distPathExists ||
+        (graphDistPath &&
+          fs.statSync(path.join(graphDistPath, "static")).isDirectory())
+      ) {
+        if (!distPathExists) {
+          console.log("checkGraphDistPathExist: true");
+        }
+        distPathExists = true;
+        return true;
+      }
+    } catch (err) {
+      console.log("checkGraphDistPathExist: false");
+      return false;
+    }
+  }
+
+  function checkGraphPublishing() {
+    try {
+      if (
+        fs
+          .statSync(path.join(graphDistPath, "static", "js", "publishing"))
+          .isDirectory()
+      ) {
+        console.log("publishing ...");
+        return true;
+      }
+    } catch (err) {
+      console.log("publishing ... done");
+      return false;
+    }
+  }
+
+  if (!graphFolderExists) {
+    console.log(`Provided graph folder ${graphPath} doesn't exist!`);
+    process.exit(1);
+  }
+
   const electronApp = await electron.launch({
     cwd: "./public/static",
     args: ["--disable_splash_screen", "electron.js"],
@@ -79,83 +82,94 @@ async function main() {
   const context = electronApp.context();
   await context.tracing.start({ screenshots: true, snapshots: true });
 
-  const page = await electronApp.firstWindow();
+  let exportSuccess = false;
 
-  page.once("load", async () => {
-    console.log("Page loaded!");
-  });
+  try {
+    const page = await electronApp.firstWindow();
 
-  await page.waitForLoadState("domcontentloaded");
-  await page.waitForFunction('window.document.title != "Loading"');
+    page.once("load", async () => {
+      console.log("Page loaded!");
+    });
 
-  // set next open directory
-  page.evaluate(
-    ([graphPath]) => {
-      Object.assign(window, {
-        __MOCKED_OPEN_DIR_PATH__: graphPath,
-      });
-    },
-    [graphPath]
-  );
+    await page.waitForLoadState("domcontentloaded");
+    await page.waitForFunction('window.document.title != "Loading"');
 
-  await page.waitForSelector("#head");
+    // set next open directory
+    page.evaluate(
+      ([graphPath]) => {
+        Object.assign(window, {
+          __MOCKED_OPEN_DIR_PATH__: graphPath,
+        });
+      },
+      [graphPath]
+    );
 
-  const hasOpenButton = await page.$("a.button >> span:has-text('Open')");
-  if (hasOpenButton) {
-    await page.click("#head >> .button >> text=Open", { force: true });
-  } else {
-    if (!(await page.$(".ls-left-sidebar-open"))) {
-      await page.click(".cp__header-left-menu.button", { force: true });
+    await page.waitForSelector("#head");
+
+    const hasOpenButton = await page.$("a.button >> span:has-text('Open')");
+    if (hasOpenButton) {
+      await page.click("#head >> .button >> text=Open", { force: true });
+    } else {
+      if (!(await page.$(".ls-left-sidebar-open"))) {
+        await page.click(".cp__header-left-menu.button", { force: true });
+      }
+      await page.click("#left-sidebar >> #repo-switch");
+      await page.click("text=Add new graph");
+      await page.waitForSelector('h1:has-text("Open a local directory")');
+      await page.click('h1:has-text("Open a local directory")');
     }
-    await page.click("#left-sidebar >> #repo-switch");
-    await page.click("text=Add new graph");
-    await page.waitForSelector('h1:has-text("Open a local directory")');
-    await page.click('h1:has-text("Open a local directory")');
-  }
 
-  await page.waitForTimeout(3000); // ?
+    await page.waitForTimeout(3000); // ?
 
-  // Parsing files
-  await page.waitForSelector(':has-text("Parsing files")', {
-    state: "hidden",
-    timeout: 1000 * 60 * 15, // 15 minutes
-  });
+    // Parsing files
+    await page.waitForSelector(':has-text("Parsing files")', {
+      state: "hidden",
+      timeout: 1000 * 60 * 15, // 15 minutes
+    });
 
-  await page.waitForFunction('window.document.title != "Loading"');
+    await page.waitForFunction('window.document.title != "Loading"');
 
-  console.log("Graph loaded for " + graphPath);
+    console.log("Graph loaded for " + graphPath);
 
-  page.evaluate(
-    ([graphDistPath]) => {
-      Object.assign(window, {
-        __MOCKED_OPEN_DIR_PATH__: graphDistPath,
-      });
-    },
-    [graphDistPath]
-  );
+    page.evaluate(
+      ([graphDistPath]) => {
+        Object.assign(window, {
+          __MOCKED_OPEN_DIR_PATH__: graphDistPath,
+        });
+      },
+      [graphDistPath]
+    );
 
-  await page.click(".button >> i.ti-dots");
-  await page.click("a.menu-link >> text=Export graph");
-  await page.click(`a:text("Export public pages")`);
+    await page.click(".button >> i.ti-dots");
+    await page.click("a.menu-link >> text=Export graph");
+    await page.click(`a:text("Export public pages")`);
 
-  await page.waitForTimeout(1000);
-
-  let TTT = 30;
-  while (!checkGraphDistPathExist() || checkGraphPublishing()) {
     await page.waitForTimeout(1000);
-    TTT--;
-    if (TTT === 0) {
+
+    let TTT = 30;
+    while (!checkGraphDistPathExist() || checkGraphPublishing()) {
+      await page.waitForTimeout(1000);
+      TTT--;
+      if (TTT === 0) {
+        throw "Export Timeout";
+      }
+    }
+    exportSuccess = true;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (argv.trace) {
       await context.tracing.stop({ path: traceFile });
-      console.log("export timeout");
+      console.log("Trace file saved to " + traceFile);
+    }
+    if (exportSuccess) {
+      console.log("Graph exported. closing ....");
+      process.exit(0);
+    } else {
+      console.log("Export failed");
       process.exit(1);
     }
   }
-
-  await page.waitForTimeout(1000);
-
-  await context.tracing.stop({ path: traceFile });
-  console.log("Graph exported. closing ....");
-  process.exit(0);
 }
 
 main();
