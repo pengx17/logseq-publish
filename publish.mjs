f0fca4c233031edfca5eb5b022422809e1aed6a9
Peng Xiao <pengxiao@outlook.com>
2022-01-15 23:58:31 +0800

    Update publish.mjs

--- publish.mjs
+++ publish.mjs
@@ -14,7 +14,7 @@ async function main() {
     .option("trace", {
       alias: "t",
       type: "boolean",
-      default: true
+      default: true,
     })
     .parse();
 
@@ -159,6 +159,7 @@ async function main() {
   } catch (err) {
     console.error(err);
   } finally {
+    await electronApp.close();
     if (argv.trace) {
       await context.tracing.stop({ path: traceFile });
       console.log("Trace file saved to " + traceFile);
